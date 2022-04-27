7c9902766a7d5467eb198727c56e3399ed1ea23b
d28035203 <289484834+d28035203@users.noreply.github.com>
2022-04-27 16:41:08 +0530

    add license

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Ship-It Script
 
-Bash **deploy helper** with env files, dry-run, and release dirs.
+Bash deploy helper: per-environment config, dry-run mode, timestamped release directories, and a `current` symlink.
+
+## Usage
 
 ```bash
 ./ship.sh dev --dry-run
 ./ship.sh staging
+./ship.sh prod --dry-run
 ```
 
-MIT · 2022 · release engineering
+## Config
+
+| File | Fields |
+|------|--------|
+| `env/dev.env` | `APP_NAME`, `DEPLOY_USER`, `DEPLOY_HOST`, `APP_PATH` |
+| `env/staging.env` | same |
+| `env/prod.env` | same |
+
+Remote `rsync` / `systemctl` commands are printed (and recorded under dry-run) so you can plug in real hosts without committing secrets.
+
+## License
+
+MIT
--- ship.sh
+++ ship.sh
@@ -1,38 +1,52 @@
 #!/usr/bin/env bash
-# ship-it-script — tiny deploy orchestrator
+# ship-it-script — env-aware deploy helper with dry-run and release dirs
 set -euo pipefail
 
-ENV="${1:-}"
+ENV_NAME="${1:-}"
 DRY=0
 [[ "${2:-}" == "--dry-run" ]] && DRY=1
 
 usage() {
-  echo "usage: $0 <dev|staging|prod> [--dry-run]"
+  echo "usage: $0 <dev|staging|prod> [--dry-run]" >&2
   exit 1
 }
 
-[[ -z "$ENV" ]] && usage
-[[ -f "env/${ENV}.env" ]] || { echo "missing env/${ENV}.env"; exit 1; }
+[[ -n "$ENV_NAME" ]] || usage
+[[ -f "env/${ENV_NAME}.env" ]] || { echo "missing env/${ENV_NAME}.env" >&2; exit 1; }
+
 # shellcheck disable=SC1090
-source "env/${ENV}.env"
+source "env/${ENV_NAME}.env"
+
+: "${APP_NAME:?}"
+: "${DEPLOY_USER:?}"
+: "${DEPLOY_HOST:?}"
+: "${APP_PATH:?}"
 
 run() {
   if [[ "$DRY" -eq 1 ]]; then
-    echo "DRY: $*"
+    printf 'DRY  %s\n' "$*"
   else
-    echo "+ $*"
+    printf '+ %s\n' "$*"
+    # shellcheck disable=SC2086
     eval "$@"
   fi
 }
 
 ts="$(date +%Y%m%d%H%M%S)"
-release_dir="releases/${ENV}/${ts}"
+release_dir="releases/${ENV_NAME}/${ts}"
+mkdir -p "releases/${ENV_NAME}"
 
-echo "== shipping ${APP_NAME} to ${ENV} =="
+echo "== shipping ${APP_NAME} → ${ENV_NAME} (${DEPLOY_USER}@${DEPLOY_HOST}) =="
+
 run "mkdir -p '${release_dir}'"
-run "echo '${ts}' > '${release_dir}/VERSION'"
-run "echo deploy user=${DEPLOY_USER} host=${DEPLOY_HOST}"
-run "echo 'rsync -az ./ ${DEPLOY_USER}@${DEPLOY_HOST}:${APP_PATH}/'"
-run "echo 'ssh ${DEPLOY_USER}@${DEPLOY_HOST} systemctl restart ${APP_NAME}'"
-run "ln -sfn '${release_dir}' 'releases/${ENV}/current'"
+run "printf '%s\n' '${ts}' > '${release_dir}/VERSION'"
+run "printf '%s\n' '${ENV_NAME}' > '${release_dir}/ENV'"
+
+# Simulated remote steps — replace with real rsync/ssh when you have hosts
+run "printf 'rsync -az --delete ./ %s@%s:%s/releases/%s/\n' '${DEPLOY_USER}' '${DEPLOY_HOST}' '${APP_PATH}' '${ts}'"
+run "printf 'ssh %s@%s \"ln -sfn %s/releases/%s %s/current && systemctl restart %s\"\n' \
+  '${DEPLOY_USER}' '${DEPLOY_HOST}' '${APP_PATH}' '${ts}' '${APP_PATH}' '${APP_NAME}'"
+
+run "ln -sfn '${release_dir}' 'releases/${ENV_NAME}/current'"
+echo "release recorded at ${release_dir}"
 echo "done."
